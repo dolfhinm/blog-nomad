5d83e4561c3edfd842d668b516c5c90ebd0da569
dolfhinm <dolfhinm@gmail.com>
2025-10-07 11:39:50 +0300

    fixed 5

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
+
 export const metadata = { title: 'PBN NeoStyle' }
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
